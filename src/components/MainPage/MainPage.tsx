60eb151a57576ea248742050fa3fc933bf167125
kielon <mosadowski23@gmail.com>
2023-04-16 17:04:58 +0200

    removed console log and comments from mainpage

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -4,15 +4,11 @@ import { Word } from "../../types";
 import SearchBar from "../SearchBar";
 import UpperBar from "./UpperBar";
 import NoWord from "./Word/NoWord";
-import Source from "./Word/Source";
 import WordContent from "./Word/WordContent";
 
 export const MainPage = () => {
   const [searchQuery, setSearchQuery] = useState<string>("");
   const [word, setWord] = useState<Word | null>(null);
-
-  console.log(word);
-  // const audo = new Audio(audioUrl);
 
   const fetchWord = async () => {
     const result = await getWord(searchQuery);
@@ -22,15 +18,8 @@ export const MainPage = () => {
     setWord(result[0]);
   };
 
-  //get audio from word
-
-  // const playAudio = () => {
-  //   audo.play();
-  // };
-
   return (
     <div className="container mx-auto px-4">
-      {/* <button onClick={playAudio}>test audio</button> */}
       <UpperBar />
       <SearchBar
         searchQuery={searchQuery}
